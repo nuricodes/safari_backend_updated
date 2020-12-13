ac8ae5ef2eccb8410c5da9abb932ab8dcf3146f0
ayninur <nuralaynabdulqadir@gmail.com>
2020-12-13 03:13:20 -0500

    cors updated

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com'
+    origins 'https://safari-resturaunt-boston.herokuapp.com/api/v1/items.json'
 
-    resource 'https://safari-resturaunt-boston.herokuapp.com/api/v1/items.json',
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
